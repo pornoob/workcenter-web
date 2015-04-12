c4435dffe64e4328fc7aeff2d08d8847862520fe
Claudio Olivares <claudio@cosof.cl>
2015-04-12 15:27:55 -0300

    Se agregan componentes vacíos por ahora, commit parcial

---
+++ src/main/java/workcenter/negocio/consulta_doc/LogicaConsultaDoc.java
@@ -0,0 +1,7 @@
+package workcenter.negocio.consulta_doc;
+
+/**
+ * Created by claudio on 11-04-15.
+ */
+public class LogicaConsultaDoc {
+}
---
+++ src/main/java/workcenter/presentacion/consulta_doc/MantenedorConsultaDoc.java
@@ -0,0 +1,13 @@
+package workcenter.presentacion.consulta_doc;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by claudio on 11-04-15.
+ */
+@Component
+@Scope("flow")
+public class MantenedorConsultaDoc {
+
+}
