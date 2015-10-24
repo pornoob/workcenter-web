684bb97f1707875adc2e5e938f0aa18107c10828
Lorenzo Veas <lorenzoavv@gmail.com>
2015-10-24 12:37:02 -0300

    se agrega el metadato dinero_

---
+++ src/main/java/workcenter/entidades/Dinero_.java
@@ -0,0 +1,15 @@
+package workcenter.entidades;
+
+        import javax.persistence.metamodel.SingularAttribute;
+        import javax.persistence.metamodel.StaticMetamodel;
+        import java.util.Date;
+
+/**
+ * Created by renholders on 24-10-2015.
+ */
+@StaticMetamodel(Dinero.class)
+public class Dinero_ {
+    public static volatile SingularAttribute<Dinero,Personal> receptor;
+    public static volatile SingularAttribute<Dinero,Concepto> concepto;
+    public static volatile SingularAttribute<Dinero,Date> fechaReal;
+}
