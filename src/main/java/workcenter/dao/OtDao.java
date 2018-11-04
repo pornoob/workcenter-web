a27b69f4424b84df0c4ee5d313e2dfb4e78c108c
Claudio Olivares <claudio.pol.olivares@gmail.com>
2018-11-04 15:32:49 -0300

    progreso temporal

--- src/main/java/workcenter/dao/OtDao.java
+++ src/main/java/workcenter/dao/OtDao.java
@@ -28,7 +28,18 @@ public class OtDao extends MyDao {
     }
 
     public List<OrdenTrabajo> findByStatus(Integer status) {
-        Query q = em.createNamedQuery("OrdenTrabajo.findByStatus", OrdenTrabajo.class);
+        StringBuilder jpql = new StringBuilder();
+        jpql.append("SELECT DISTINCT ot FROM OrdenTrabajo ot ")
+                .append("INNER JOIN FETCH ot.solicitante s ")
+                .append("INNER JOIN FETCH ot.trazabilidad tot ")
+                .append("LEFT JOIN FETCH tot.ejecutor ")
+                .append("WHERE EXISTS ( ")
+                .append("    SELECT o FROM OrdenTrabajo o ")
+                .append("    INNER JOIN o.trazabilidad to ")
+                .append("    WHERE to.fecha = ")
+                .append("    (SELECT MAX(tmo.fecha) FROM TrazabilidadOt tmo WHERE tmo.ot = o) ")
+                .append("    AND to.estadoId = :status AND ot.id = o.id )");
+        Query q = em.createQuery(jpql.toString(), OrdenTrabajo.class);
         q.setParameter("status", status);
         return q.getResultList();
     }
@@ -61,6 +72,7 @@ public class OtDao extends MyDao {
                 .append("LEFT JOIN FETCH ot.asistentes ")
                 .append("LEFT JOIN FETCH ot.trazabilidad ")
                 .append("LEFT JOIN FETCH ot.mantencionTracto mt ")
+                .append("LEFT JOIN FETCH mt.tipo ")
                 .append("LEFT JOIN FETCH ot.mantencionMaquina mm ")
                 .append("LEFT JOIN FETCH ot.mantencionSemirremolque ms ")
                 .append("LEFT JOIN FETCH mt.tracto t ")
